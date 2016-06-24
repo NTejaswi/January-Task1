d31a2acdf2ed5a37f76eddabe9d7ef66e4776d0c
nue <peter.szatmary@gmail.com>
2016-06-24 23:02:36 +0200

    Initial commit. Spring boot demo application with Mustache logic less templates integrated 3 party but simple springmvc-mustache. With Lombok POJOs stay simple readable and small.

---
+++ src/test/java/com/szatmary/peter/mustache/demo/SpringBootRxJavaReactApplicationTests.java
@@ -0,0 +1,19 @@
+package com.szatmary.peter.mustache.demo;
+
+import com.szatmary.peter.mustache.demo.SpringBootMustacheDemoApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringBootMustacheDemoApplication.class)
+@WebAppConfiguration
+public class SpringBootRxJavaReactApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
